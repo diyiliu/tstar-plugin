6a95a3d531a28076d711b48b840bb6e2c08ad37c
DIYILIU <572772828@qq.com>
2019-05-29 15:57:36 +0800

    修改工况存库格式

--- src/main/java/com/tiza/plugin/util/JacksonUtil.java
+++ src/main/java/com/tiza/plugin/util/JacksonUtil.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.*;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description: JacksonUtil
@@ -43,6 +45,24 @@ public class JacksonUtil {
 
     public static <T> T  toObject(String content, Class<T> clazz) throws IOException {
 
-        return (T) mapper.readValue(content, clazz);
+        return mapper.readValue(content, clazz);
+    }
+
+    public static List toList(String content, Class clazz) throws IOException {
+        JavaType javaType = getCollectionType(ArrayList.class, clazz);
+
+        return mapper.readValue(content, javaType);
+    }
+
+    /**
+     * 获取泛型的Collection Type
+     *
+     * @param collectionClass 泛型的Collection
+     * @param elementClasses  元素类
+     * @return JavaType Java类型
+     * @since 1.0
+     */
+    public static JavaType getCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {
+        return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
     }
 }
